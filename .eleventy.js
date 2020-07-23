615bed82c514ac5a66f0d69702b1ade93319fc7f
Dak <dakahn@protonmail.com>
2020-07-23 02:25:48 -0500

    add minified css

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,18 @@
-const moment = require('moment');
- 
-moment.locale('en');
- 
+const moment = require("moment");
+const CleanCSS = require("clean-css");
+
+moment.locale("en");
+
 module.exports = function (eleventyConfig) {
- 
-  eleventyConfig.addFilter('dateIso', date => {
+  eleventyConfig.addFilter("dateIso", (date) => {
     return moment(date).toISOString();
   });
- 
-  eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+
+  eleventyConfig.addFilter("dateReadable", (date) => {
+    return moment(date).utc().format("LL"); // E.g. May 31, 2019
+  });
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
   });
 };
