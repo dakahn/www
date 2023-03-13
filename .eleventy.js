c6238124e2146994bafc12fe2e4c0c09921e94b4
dakahn <dakahn@protonmail.com>
2023-03-13 17:58:48 -0500

    upgrade to 11ty 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,11 @@
 const moment = require("moment");
 const CleanCSS = require("clean-css");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const {
-  DateTime
-} = require("luxon");
+const { DateTime } = require("luxon");
 
 moment.locale("en");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("dateIso", (date) => {
     return moment(date).toISOString();
   });
@@ -16,7 +14,7 @@ module.exports = function(eleventyConfig) {
     return moment(date).utc().format("LL"); // E.g. May 31, 2019
   });
 
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
   eleventyConfig.addFilter("postDate", (dateObj) => {
